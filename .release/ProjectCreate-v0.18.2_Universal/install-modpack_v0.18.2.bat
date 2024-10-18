206a3337d69f0d17ec16e6511c3b8c7eb1f78b59
MisterNoNameLP <misternonamelp@web.de>
2024-10-18 21:10:31 +0200

    v0.18.2_prep

---
+++ .release/ProjectCreate-v0.18.2_Universal/install-modpack_v0.18.2.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar packwiz-installer-bootstrap.jar -s client https://github.com/OpenPlayVerse/ProjectCreate/raw/6da227685db8dcccab1ca15293d4f220694a19e2/packwiz/pack.toml
---
+++ .release/ProjectCreate-v0.18.2_Universal/install-modpack_v0.18.2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar packwiz-installer-bootstrap.jar -s client https://github.com/OpenPlayVerse/ProjectCreate/raw/6da227685db8dcccab1ca15293d4f220694a19e2/packwiz/pack.toml
